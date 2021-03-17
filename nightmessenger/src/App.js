d8ed65a7ae49942a52d63d8bc32fc0b291dee0e9
Kevin Rodriguez <rodriguez.kevin.094@gmail.com>
2021-03-16 22:05:46 -0400

    added useState

---
+++ nightmessenger/src/App.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+import './App.css';
+
+function App() {
+
+  const [input, setInput] = useState('')
+
+  return (
+    <div className="App">
+        <h1>NightMessenger</h1>
+        <input/>
+        <button></button>
+    </div>
+  );
+}
+
+export default App;
