65d09516f275c21f5e318686eea0624a6353ca83
Kevin Rodriguez <rodriguez.kevin.094@gmail.com>
2021-03-16 23:02:10 -0400

    added message uses

--- nightmessenger/src/App.js
+++ nightmessenger/src/App.js
@@ -4,12 +4,27 @@ import './App.css';
 function App() {
 
   const [input, setInput] = useState('')
+  const [messages, setMessages] = useState(['hello', 'hi'])
+  console.log(input);
+  console.log(messages);
+  
+  const sendMessage = (e) => {
+    setMessages([...messages, input]);
+    setInput('');
+  }
 
   return (
     <div className="App">
-        <h1>NightMessenger</h1>
-        <input/>
-        <button></button>
+      <h1>NightMessenger</h1>
+      <input value={input} onChange={e => setInput(e.target.value)}/>
+      <button onClick={sendMessage}>Send</button>
+
+      {
+        messages.map(message => (
+          <p>{message}</p>
+        ))
+      }
+
     </div>
   );
 }
