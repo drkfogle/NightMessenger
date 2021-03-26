8be073f7975a829d438f48a18cf1a3b19dde5553
Kevin Rodriguez <rodriguez.kevin.094@gmail.com>
2021-03-25 21:53:06 -0400

    added messagesusers and messageguests

--- nightmessenger/src/App.js
+++ nightmessenger/src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import {Button, FormControl, InputLabel, Input} from '@material-ui/core'
 import Message from './Message'
 import './App.css';
@@ -6,32 +6,36 @@ import './App.css';
 function App() {
 
   const [input, setInput] = useState('')
-  const [messages, setMessages] = useState([])
-  console.log(input);
-  console.log(messages);
+  const [messages, setMessages] = useState([
+  {user: 'jesus', text:'hey'}, 
+  {user: 'gary', text:'sup'}, 
+  {user: 'mike', text:'hello'}])
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    setUser(prompt('Please enter UserID'))
+  }, [])
   
   const sendMessage = (e) => {
     e.preventDefault();
-    
-    setMessages([...messages, input]);
-    setInput('');
+    setMessages([...messages, {user: user, text: input}
+    ]);
+    setInput("");
   }
 
   return (
     <div className="App">
       <h1>NightMessenger</h1>
-      <FormControl>
-        <InputLabel>Enter message here</InputLabel>
-        <Input value={input} onChange={e => setInput(e.target.value)}/>
-        <Button onClick={sendMessage} type='submit' variant='contained' color='secondary' disabled={!input}>Send</Button>
-      </FormControl>
+      <h2>Welcome {user}</h2>
 
-      {
-        messages.map(message => (
-          <Message text={message} />
-        ))
-      }
-
+      <form>
+        <FormControl>
+          <InputLabel>Enter message here</InputLabel>
+          <Input value={input} onChange={(e) => setInput(e.target.value)}/>
+          <Button onClick={sendMessage} type="submit" variant='contained' color='secondary' disabled={!input}>Send</Button>
+        </FormControl>
+      </form>  
+      {messages.map(message => (<Message user={user} message={message} /> ))}
     </div>
   );
 }
--- nightmessenger/src/Message.js
+++ nightmessenger/src/Message.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import { Card, CardContent, Typography } from '@material-ui/core';
+import React from 'react';
+import './Message.css' ;
 
-function Message(props) {
+
+function Message({message, user}) {
+  const isUser = user === message.user
+
   return (
-    <div>
-      <h2>{props.text}</h2>
+    <div className={`message ${isUser && "message__user"}`}>
+      <Card className={isUser ? "message__userCard" : "message__guestCard" }>
+        <CardContent>
+          <Typography 
+            color="white"
+            variant="h5"
+            component="h2"
+          >
+            {message.user}: {message.text}
+          </Typography>
+        </CardContent>
+      </Card>
     </div>
   )
 }
